db0ed83925adba91951ea5b9ea923f9c126d00ec
Leki <lekisintole82@gmail.com>
2024-06-15 00:10:25 +0300

    table attributes

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -41,11 +41,11 @@ class CityResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('state.name')
+                       ->numeric()
+                      ->sortable(),
                 Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('state_id')
-                    ->numeric()
-                    ->sortable(),
+                ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
