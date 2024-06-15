8730f7c8fafccbce98584175c406004d15746458
Leki <lekisintole82@gmail.com>
2024-06-15 10:52:19 +0300

    Model relationship to country

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name','code','phonecode'
+    ];
+    public function employees(): HasMany{
+        return $this->hasMany(Employee::class);
+    }
+    public function states() :HasMany {
+        return $this->hasMany(State::class);
+    }
+
 }
